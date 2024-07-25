da694ca53aff2ceec08b0b42bc3cd892a92ac8e7
Debojyoti Tantra <tantradebojyoti@gmail.com>
2024-07-25 23:56:59 +0530

    Add files via upload

---
+++ Chapter 3/01_example.py
@@ -0,0 +1,17 @@
+a = 'Debojyoti'  # single code string
+b = "Tantra"   # double code string
+c = '''Debojyoti
+Tantra'''   # multiline string
+# we cann't change ani element of string
+
+# in string a
+# index of D is 0 or -9
+# index of e is 1 or -8
+# index of b is 2 or -7
+# index of o is 3 or -6
+# index of j is 4 or -5
+# index of y is 5 or -4
+# index of o is 6 or -3
+# index of t is 7 or -2
+# index of i is 8 or -1
+# length of the string 9
