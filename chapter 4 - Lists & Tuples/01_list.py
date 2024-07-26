71c8fbbfc1a88368ff00cb6570ece2b9eda96fa2
Debojyoti Tantra <tantradebojyoti@gmail.com>
2024-07-26 22:58:07 +0530

    Add files via upload

---
+++ chapter 4 - Lists & Tuples/01_list.py
@@ -0,0 +1,12 @@
+everything = ["orange", "apple", 5.67, 6, "rohan"]
+print(everything)
+print(everything[0])
+
+everything[0] = "green"
+print(everything)
+print(everything[0])   # unlike strings, lists are mutable
+
+
+# LIST INDEXING
+# list indexing is same as string
+print(everything[1:4:2])
