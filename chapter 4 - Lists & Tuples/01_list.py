d7a50f821069949e50fbe3a132d59fa2dd261a6c
Debojyoti Tantra <tantradebojyoti@gmail.com>
2024-07-26 23:01:49 +0530

    Update 01_list.py

--- chapter 4 - Lists & Tuples/01_list.py
+++ chapter 4 - Lists & Tuples/01_list.py
@@ -1,12 +1,13 @@
 everything = ["orange", "apple", 5.67, 6, "rohan"]
-print(everything)
-print(everything[0])
+print(everything)   # output: ['orange', 'apple', 5.67, 6, 'rohan']
+print(everything[0])   # output: orange
 
 everything[0] = "green"
-print(everything)
-print(everything[0])   # unlike strings, lists are mutable
+print(everything)   # output: ['green', 'apple', 5.67, 6, 'rohan']
+print(everything[0])   # output: green
+# unlike strings, lists are mutable
 
 
 # LIST INDEXING
 # list indexing is same as string
-print(everything[1:4:2])
+print(everything[1:4:2])   # output: ['apple', 6]
