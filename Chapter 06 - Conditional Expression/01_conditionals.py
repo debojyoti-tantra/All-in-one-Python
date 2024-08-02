aaa7ae36d3dfe1f2a6cacf10e9cde794d9b0bf73
Debojyoti Tantra <tantradebojyoti@gmail.com>
2024-08-03 00:39:04 +0530

    Create 01_conditionals.py

---
+++ Chapter 06 - Conditional Expression/01_conditionals.py
@@ -0,0 +1,17 @@
+age = int(input("Enter your age: "))
+
+# if-else statement
+if (age>=18):
+    print("you are adult.")
+else:
+    print("You are child")
+
+# if-elif-else ladder
+if (age>18):
+    print("You are eligable for votar card")
+elif (age<0):
+    print("Not a valid age")
+else:
+    print("You are not elegible for votar card")
+
+print("Program finished")
